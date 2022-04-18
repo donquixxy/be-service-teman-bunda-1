bf7ebb79b4be203e782e0165c0bccf3274bd4cd7
tensuqiuwulu <tensu104creators98@gmail.com>
2022-04-18 13:49:50 +0800

    fix auth token

--- models/http/response/auth_login_response.go
+++ models/http/response/auth_login_response.go
@@ -1,15 +1,11 @@
 package response
 
 type AuthResponse struct {
-	Id           string `json:"id"`
-	Username     string `json:"username"`
 	Token        string `json:"token"`
 	RefreshToken string `json:"refresh_token"`
 }
 
 func ToAuthResponse(id string, username string, token string, refreshToken string) (authResponse AuthResponse) {
-	authResponse.Id = id
-	authResponse.Username = username
 	authResponse.Token = token
 	authResponse.RefreshToken = refreshToken
 	return authResponse
